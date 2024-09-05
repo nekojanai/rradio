11ed83476662ed2bf75f9928370bd1863c132f1a
nekojanai <nekojanai@debnyan.com>
2024-09-05 18:57:32 +0200

    Change station fetching

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::{
     error::Error,
-    thread,
-    time::{self, Instant},
+    fmt::Debug,
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+    thread::{self},
 };
 
-use async_std::task;
+use async_std::task::{self};
 use fltk::{
     app::{self, Receiver, Sender},
     browser::{Browser, BrowserType},
     button::Button,
-    enums::{CallbackTrigger, Color, FrameType},
+    enums::{Align, CallbackTrigger, Color, FrameType, LabelType},
     frame::Frame,
     input::Input,
     prelude::{BrowserExt, GroupExt, InputExt, WidgetBase, WidgetExt},
     text::SimpleTerminal,
     window::{DoubleWindow, Window},
 };
+use json::{JsonError, JsonValue};
 use radiobrowser::{ApiStation, RadioBrowserAPI};
-use vlc::{Instance, Media, MediaPlayer, State};
+use vlc::{Instance, Media, MediaPlayer};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
@@ -25,6 +29,7 @@ pub enum Message {
     StationsFetchedSuccess,
     FilterStations,
     PlayRequest,
+    PauseRequest,
 }
 
 #[async_std::main]
@@ -39,19 +44,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     frame.set_color(Color::Black);
 
     let (tx_message, rx_message) = app::channel::<Message>();
-    let (tx_mediastate, rx_mediastate) = app::channel::<State>();
-    let fetch_timer = Instant::now();
 
     let mut browser = build_browser(&win);
     browser.set_type(BrowserType::Hold);
     browser.add("no stations to display");
     browser.set_selection_color(Color::Magenta);
+    browser.set_label_type(LabelType::Shadow);
+    browser.set_label_color(Color::Black);
 
     let (mut search_input, mut search_button) = build_search(&win);
 
     let mut status = SimpleTerminal::default()
-        .with_size(win.width(), 40)
+        .with_align(Align::Right)
+        .with_size(win.width() - 40, 40)
         .below_of(&browser, 0);
+    status.set_pos(40, status.y());
+
+    let mut play_button = Button::new(win.width() - 40, 0, 40, 40, ">").below_of(&browser, 0);
+    let tx_message_clone = tx_message.clone();
+    play_button.set_callback(move |_| tx_message_clone.send(Message::PauseRequest));
 
     search_input.emit(tx_message, Message::FilterStations);
     search_button.emit(tx_message, Message::FilterStations);
@@ -63,27 +74,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let all_stations: &mut Option<Vec<ApiStation>> = &mut None;
 
     while app.wait() {
-        if all_stations.is_none() {
-            spawn_fetch_thread(&tx_message, all_stations);
-            fill_station_browser(&browser, all_stations);
-        }
         if let Some(msg) = rx_message.recv() {
             match msg {
                 Message::FetchStations => {
+                    spawn_fetch_thread(&browser, &tx_message, all_stations);
                     if all_stations.is_some() {
                         status.set_text(&format!(
-                            "Successfully fetched {} stations in {}",
+                            "Successfully fetched {} stations",
                             &all_stations.clone().unwrap().len(),
-                            fetch_timer.duration_since(Instant::now()).as_millis()
                         ));
                     }
-                    spawn_fetch_thread(&tx_message, all_stations);
                 }
                 Message::FilterStations => {
+                    if all_stations.is_none() {
+                        if !is_cache_present() {
+                            spawn_fetch_thread(&browser, &tx_message, all_stations);
+                            let json_vec = all_stations
+                                .unwrap()
+                                .into_iter()
+                                .map(|e| station_to_json(e).unwrap())
+                                .collect::<Vec<_>>();
+                            write_data_to_file(serde_json::to_string(&json_vec));
+                            fill_station_browser(&browser, all_stations);
+                        }
+                    }
                     search_input.set_text_color(Color::from_rgba_tuple((255, 255, 255, 50)));
                     status.set_text(&format!("..."));
-                    let filtered_stations =
-                        filter_stations(&all_stations.clone().unwrap(), &search_input.value());
+                    let filtered_stations = filter_stations(
+                        &all_stations.clone().unwrap_or(vec![]),
+                        &search_input.value(),
+                    );
                     status.set_text(&format!("Found: {}", filtered_stations.len()));
 
                     browser.clear();
@@ -91,27 +111,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         browser.add_with_data(&format_station(&station), station.clone())
                     }
                 }
-                Message::StationsFetchedSuccess => {
-                    if all_stations.is_some() {
-                        status.set_text(&format!(
-                            "Successfully fetched {} stations in {}",
-                            &all_stations.clone().unwrap().len(),
-                            fetch_timer.duration_since(Instant::now()).as_millis()
-                        ));
-                    }
-                }
+                Message::StationsFetchedSuccess => fill_station_browser(&browser, all_stations),
                 Message::PlayRequest => {
-                    browser.selected_items().iter().for_each(|&line| unsafe {
-                        let station = browser
-                            .data::<ApiStation>(line)
-                            .expect("Error selecting station");
-                        status.set_text(&format!("Playing: {}", &station.url_resolved));
-                        init_player(
-                            station.url_resolved,
-                            tx_mediastate.clone(),
-                            rx_mediastate.clone(),
-                        );
-                    });
+                    let station = unsafe {
+                        browser
+                            .data::<ApiStation>(
+                                *browser
+                                    .selected_items()
+                                    .first()
+                                    .expect("Couldn't get selected station"),
+                            )
+                            .expect("")
+                    };
+
+                    let status_text = format!("Playing: {}", &station.url_resolved);
+                    status.set_text(&status_text);
+                    println!("{}", status_text);
+                    let instance = Instance::new().expect("Error initializing vlc instance");
+                    let media = Media::new_location(&instance, &station.url_resolved)
+                        .expect("Error initializing vlc media");
+                    let player =
+                        MediaPlayer::new(&instance).expect("Error initializing vlc media player");
+                    init_player(
+                        play_button.clone(),
+                        tx_message.clone(),
+                        rx_message.clone(),
+                        player,
+                        media,
+                    );
+                }
+                Message::PauseRequest => {
+                    let _ = &status.set_text("Playback stopped.");
+                    play_button.set_label(">");
                 }
             }
         };
@@ -120,40 +151,133 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn init_player(url: String, _tx_mediastate: Sender<State>, _rx_mediastate: Receiver<State>) {
-    print!("{}", url);
-    let _player_thread = thread::spawn(move || {
-        let instance = Instance::new().expect("Error initializing vlc instance");
-        let media = Media::new_location(&instance, &url).expect("Error initializing vlc media");
-        let player = MediaPlayer::new(&instance).expect("Error initializing vlc media player");
-        player.set_media(&media);
-        player.play().expect("Error playing vlc media");
+fn station_to_json(station: ApiStation) -> Result<ApiStation, serde_json::Error> {
+    let s = serde_json::json!({
+       "changeuuid": station.changeuuid,
+       "stationuuid": station.stationuuid,
+       "serveruuid": station.serveruuid,
+       "name": station.name,
+       "url": station.url,
+       "url_resolved": station.url_resolved,
+       "homepage": station.homepage,
+       "favicon": station.favicon,
+       "tags": station.tags,
+       "country": station.country,
+       "countrycode": station.countrycode,
+       "iso_3166_2": station.iso_3166_2,
+       "state": station.state,
+       "language": station.language,
+       "languagecodes": station.languagecodes,
+       "votes": station.votes,
+       "lastchangetime_iso8601": station.lastchangetime_iso8601,
+       "codec": station.codec,
+       "bitrate": station.bitrate,
+       "hls": station.hls,
+       "lastcheckok": station.lastcheckok,
+       "lastchecktime_iso8601": station.lastchecktime_iso8601,
+       "lastcheckoktime_iso8601": station.lastcheckoktime_iso8601,
+       "lastlocalchecktime_iso8601": station.lastlocalchecktime_iso8601,
+       "clicktimestamp_iso8601": station.clicktimestamp_iso8601,
+       "lastchecktime_iso8601": station.lastchecktime_iso8601,
+       "lastcheckoktime_iso8601": station.lastcheckoktime_iso8601,
+       "lastlocalchecktime_iso8601": station.lastlocalchecktime_iso8601,
+       "clicktimestamp_iso8601": station.clicktimestamp_iso8601,
+       "clickcount": station.clickcount,
+       "clicktrend": station.clicktrend ,
+       "ssl_error": station.ssl_error,
+       "geo_lat": station.geo_lat,
+       "geo_long": station.geo_long,
+       "has_extended_info": station.has_extended_info,
+    });
+    let json_value = serde_json::from_value::<ApiStation>(s);
 
-        loop {
-            thread::sleep(time::Duration::from_millis(250));
-            if !player.is_playing() {
-                break;
+    json_value
+}
+
+fn init_player(
+    play_button: Button,
+    _tx_mediastate: Sender<Message>,
+    rx_mediastate: Receiver<Message>,
+    player: MediaPlayer,
+    media: Media,
+) {
+    player.set_media(&media);
+    player.play().expect("Error playing vlc media");
+    let play_button_ref = &mut play_button.clone();
+    play_button_ref.set_label("||");
+    let _player_thread = thread::spawn(move || loop {
+        thread::sleep(std::time::Duration::from_secs(u64::MAX));
+        match rx_mediastate.recv() {
+            Some(msg) => {
+                println!("{:?}", msg)
+            }
+            None => {
+                println!("idk fuggin chickem nuggies")
             }
         }
     });
 }
 
+fn is_cache_present() -> bool {
+    let path = Path::new("stations.json");
+    let _ = match File::open(path) {
+        Err(_) => return false,
+        Ok(_) => return true,
+    };
+}
+
+fn write_data_to_file(data: &str) {
+    let path = Path::new("stations.json");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}:{}", display, why),
+        Ok(file) => file,
+    };
+    match file.write_all(data.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("Successfully wrote to: {}", display),
+    }
+}
+
+fn read_data_from_file_and_parse() -> Result<JsonValue, JsonError> {
+    let path = Path::new("stations.json");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains: \n{}", display, s),
+    }
+    let parsed = json::parse(&s);
+
+    parsed
+}
+
 fn spawn_fetch_thread(
+    browser: &Browser,
     tx_fetch_signal: &Sender<Message>,
     stations_container: &mut Option<Vec<ApiStation>>,
 ) {
     task::block_on(async {
-        println!("start");
+        let stations = fetch_stations().await.unwrap_or(vec![]);
         tx_fetch_signal.send(Message::StationsFetchedSuccess);
-        let _ = stations_container.insert(filter_unusables(&fetch_stations().await));
+
+        let _ = stations_container.insert(stations.clone());
+        fill_station_browser(&browser, &mut Some(stations.clone()));
     });
 }
 
 fn fill_station_browser(browser: &Browser, stations: &mut Option<Vec<ApiStation>>) {
     if stations.is_some() {
+        let mut browser_ref = browser.clone();
+        if stations.clone().unwrap().len() < 1 {
+            browser_ref.add("Received 0 stations.")
+        }
+        browser_ref.clear();
         for station in stations.clone().unwrap() {
-            let mut browser_ref = browser.clone();
-            browser_ref.clear();
             browser_ref.add_with_data(&format_station(&station), station.clone());
         }
     }
@@ -181,16 +305,8 @@ fn build_browser(window: &DoubleWindow) -> Browser {
     browser
 }
 
-async fn fetch_stations() -> Vec<ApiStation> {
-    let stations = RadioBrowserAPI::new()
-        .await
-        .expect("Error initializing RadionBrowserAPI")
-        .get_stations()
-        .send()
-        .await
-        .expect("Error while fetching stations");
-
-    stations
+async fn fetch_stations() -> Result<Vec<ApiStation>, Box<dyn Error>> {
+    RadioBrowserAPI::new().await?.get_stations().send().await
 }
 
 fn filter_stations(stations: &Vec<ApiStation>, filter: &str) -> Vec<ApiStation> {
@@ -207,22 +323,16 @@ fn filter_stations(stations: &Vec<ApiStation>, filter: &str) -> Vec<ApiStation> 
         .collect::<Vec<_>>()
 }
 
-fn filter_unusables(stations: &Vec<ApiStation>) -> Vec<ApiStation> {
-    stations
-        .clone()
-        .into_iter()
-        .filter(|v| !v.url_resolved.is_empty())
-        .collect::<Vec<_>>()
-}
-
 fn format_station(station: &ApiStation) -> String {
     format!(
         "{}|{}|{}|{}",
-        if station.name.is_empty() {
+        (if station.name.is_empty() {
             station.url_resolved.to_ascii_lowercase()
         } else {
             station.name.to_ascii_lowercase()
-        },
+        })
+        .trim_end()
+        .trim_start(),
         station.state,
         station.country,
         station.tags
